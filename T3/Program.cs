7f05c5a406f0fb3a59add6e173c69cc7f3b7b95d
Jnarhi <redline069@hotmail.com>
2016-02-03 11:00:50 +0200

    yritin

---
+++ T3/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace T3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<TVshow> tvshow = new List<TVshow>();
+
+            tvshow.Add(new TVshow { Name = "Quantico", Channel = "Nelonen", Info = "Amerikkalainen jännitysdraama", Time = "21:00-22:00", });
+            tvshow.Add(new TVshow { Name = "Myytinmurtajat", Channel = "Sub", Info = "Adam ja Jamie", Time = "21:00-22:00" });
+            tvshow.Add(new TVshow { Name = "Family Guy", Channel = "FOX", Info = "Amerikkalainen animaatiosarja", Time = "17:05-17.35" });
+            tvshow.Add(new TVshow { Name = "Simpsonit", Channel = "Sub", Info = "Amerikkalainen animaatiosarja", Time = "20:00-20:30" });
+
+
+            StreamWriter outputFile = null;
+            try
+            {
+                outputFile = new StreamWriter("tvlista.txt");
+                foreach (TVshow show in tvshow)
+                {
+                    outputFile.WriteLine(tvshow.ToString());
+                }
+
+
+            }
+           
+            finally
+            {
+                if (outputFile != null)
+                {
+                    outputFile.Close();
+                }
+            }
+          
+        }
+    }
+      
+}
---
+++ T3/TVshow.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T3
+{
+    class TVshow
+    {
+        public string Name { get; set; }
+        public string Channel { get; set; }
+        public string Info { get; set; }
+        public string Time { get; set; }
+    }
+}
